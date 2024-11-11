1b4e29b754b9188de82eee621c46a81b4e5330d1
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-11-11 06:37:50 +0200

    chore:🌻 learnig the Result enum and match statement, loop

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
+use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
-use rand::Rng;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(0..=1000);
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => {println!("You win!");}
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(numb) => numb,
+            Err(_) => continue,
+        };
+        println!("Please input your guess.");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+        println!("You guessed: {guess}");
     }
-
-    println!("You guessed: {guess}, and The secret number is {secret_number}");
 }
