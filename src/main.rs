e3084717659846b989d13c5e5e7304f55d4f77c2
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-12-13 15:10:12 +0200

    feat: added tokio

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,28 @@
-use std::fmt::{Display, Formatter};
-use std::io::Read;
-use std::fs;
-use std::ops::Deref;
-use std::thread::sleep;
-use std::time::Duration;
-use crate::chapters::traits::News;
+use tokio::{spawn, time::{sleep, Duration}};
 
-mod chapters;
+async fn say_hello() {
+    // Wait for a while before printing to make it a more interesting race.
+    sleep(Duration::from_millis(200)).await;
+    println!("hello");
+}
+
+async fn say_world() {
+    sleep(Duration::from_millis(200)).await;
+    println!("world!");
+}
 
 #[tokio::main]
 async fn main() {
-    let a = wait_to_add(2,3).await;
-    println!("Hello, world!, {:#?}",   a);
-}
+    spawn(say_hello());
+    spawn(say_world());
 
-async fn wait_to_add(a:u32, b:u32)->u32{
-    println!("Wait");
-    sleep(Duration::from_secs(3));
-    println!("After {:?}", a);
-    a+b
-}
-#[derive(Debug)]
-struct Post {
-    title: String,
-    author: String,
-    comment: String,
-    content: String,
-}
+    let a =spawn(say_hello());
+    let b =spawn(say_world());
 
-impl News for Post {
-    fn read(self) -> Post{
-        self
-    }
-
-    fn comments() -> String {
-        String::from("Iancu")
-    }
-}
-
-struct MyConfig<T> {
-    dry_voltage: T,
-    wet_voltage: T,
-    moisture_percentage: T,
-}
-
-impl<T: Default> MyConfig<T> {
-    fn new(dry_voltage: T, wet_voltage: T) -> MyConfig<T> {
-        MyConfig{
-            dry_voltage,
-            wet_voltage,
-            moisture_percentage: T::default()
-        }
-    }
-}
-
-impl<T> Display for MyConfig<T> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        println!("Dry voltage: {}", self);
-
-        todo!()
-    }
-}
-
-impl <T> Deref for MyConfig<T> {
-    type Target = T;
-    
-    fn deref(&self) -> &Self::Target {
-        &self.dry_voltage
-    }
-}
-
-fn say_something<T : Display>(something: T) {
-    println!("I am saying {}", something);
+    let _ = a.await;
+    let _ = b.await;
+    println!("Final before sleep in main");
+    // Wait for a while to give the tasks time to run.
+    sleep(Duration::from_millis(100)).await;
+    println!("Final after sleep in main");
 }
